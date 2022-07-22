fd7fad15cb2fd8f6cfd809694f5bf3921031c7a7
TylerLott <tylerlott0126@gmail.com>
2022-07-22 09:45:37 -0600

    init

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 d3.select("svg")
 async function init() {
-  const data = await d3.csv("D:/School/416_data_vis/FinalProj/inflation.csv")
+  const data = await d3.csv("https://TylerLott.github.io/inflation.csv")
   console.log(data)
   console.log("here")
 }
